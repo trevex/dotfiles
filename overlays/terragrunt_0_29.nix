910f00124f445d003eaef5e30c10551bb2799533
Niklas Voss <niklas.voss@engelvoelkers.com>
2021-07-09 23:31:05 +0200

    Add overlays

---
+++ overlays/terragrunt_0_29.nix
@@ -0,0 +1,27 @@
+final: prev: {
+  terragrunt_0_29 = { lib, buildGoModule, fetchFromGitHub }: buildGoModule rec {
+    pname = "terragrunt";
+    version = "0.29.10";
+
+    src = fetchFromGitHub {
+      owner = "gruntwork-io";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "sha256-JRIwPOExPbwLS7ps4pJpvIRaZ9jZZjVK+POaUHAmiPI=";
+    };
+
+    vendorSha256 = "sha256-lck4nabDhFA8N0lo+cIKiJjlg2TGx3qMExbblHQwbvQ=";
+
+    doCheck = false;
+
+    ldflags = [ "-s" "-w" "-X main.VERSION=v${version}" ];
+
+    doInstallCheck = true;
+    installCheckPhase = ''
+      runHook preInstallCheck
+      $out/bin/terragrunt --help
+      $out/bin/terragrunt --version | grep "v${version}"
+      runHook postInstallCheck
+    '';
+  };
+}
