3ecaefebdc410a138c6729b5c578fcadca69207e
Niklas Voss <niklas.voss@gmail.com>
2021-10-25 11:19:10 +0200

    Use different lockscreen, fix neovim warnings

--- profiles/neovim/init.lua
+++ profiles/neovim/init.lua
@@ -146,8 +146,7 @@ require "bufferline".setup {
     view = "multiwindow",
     show_buffer_close_icons = true,
     show_close_icon = false,
-    separator_style = "slant",
-    mappings = "true"
+    separator_style = "slant"
   }
 }
 
@@ -157,20 +156,13 @@ g.nvim_tree_width = 25
 g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 g.nvim_tree_gitignore = 1
 g.nvim_tree_auto_ignore_ft = {"dashboard"} -- don't open tree on specific fiypes.
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 1 -- closes tree when it's the last window
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
-g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_tab_open = 0
 g.nvim_tree_allow_resize = 1
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_disable_netrw = 1
-g.nvim_tree_hijack_netrw = 0
-g.nvim_tree_update_cwd = 1
 
 g.nvim_tree_show_icons = {
   git = 1,
@@ -202,6 +194,23 @@ g.nvim_tree_icons = {
 map("n", "<C-T>", ":NvimTreeToggle<CR>", {noremap=true})
 map("n", "<leader>r", ":NvimTreeRefresh<CR>", {noremap=true})
 map("n", "<leader>n", ":NvimTreeFindFile<CR>", {noremap=true})
+
+require'nvim-tree'.setup {
+  disable_netrw = true,
+  hijack_netrw = false,
+  auto_close = true, -- closes tree when it's the last window
+  open_on_tab = false,
+  update_to_buf_dir   = {
+    -- enable the feature
+    enable = true,
+    -- allow to open the tree if it was previously closed
+    auto_open = true,
+  },
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+  },
+}
 
 
 -- Telescope
