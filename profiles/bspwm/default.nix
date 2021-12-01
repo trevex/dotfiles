1aa889b4d565a68c538b95405e520f0f04b069d8
Niklas Voss <niklas.voss@gmail.com>
2021-12-01 11:24:48 +0100

    Fix lockscreen that was broken caused by renamed flags of i3lock-color

--- profiles/bspwm/default.nix
+++ profiles/bspwm/default.nix
@@ -22,7 +22,7 @@ let
   autoPresel = pkgs.writeScriptBin "auto-presel" ''
     #!${pkgs.stdenv.shell}
     ${builtins.readFile ./scripts/auto-presel}
-    '';
+  '';
   windowPromoter = pkgs.writeScriptBin "window-promoter" ''
     #!${pkgs.stdenv.shell}
     ${builtins.readFile ./scripts/window-promoter}
@@ -30,12 +30,13 @@ let
   windowResize = pkgs.writeScriptBin "window-resize" ''
     #!${pkgs.stdenv.shell}
     ${builtins.readFile ./scripts/window-resize}
-    '';
+  '';
   whid = pkgs.writeScriptBin "whid" ''
     #!${pkgs.stdenv.shell}
     ${builtins.readFile ./scripts/whid}
   '';
-in {
+in
+{
   # TODO: move some of the fundamental X11 stuff out...
 
   services.acpid.enable = true;
@@ -82,7 +83,7 @@ in {
     ranger
   ]);
 
-  my.home = { config, ... } : {
+  my.home = { config, ... }: {
     # Let's also install some convenience tools to configure gtk etc.
     home.packages = with pkgs; [
       lxappearance
@@ -162,7 +163,11 @@ in {
     services.screen-locker = {
       enable = true;
       inactiveInterval = 300;
-      lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color -n -c 1d2021 --indicator -k --timecolor=#ebdbb2ff --datecolor=#ebdbb2ff --insidecolor=#1d2021cc --insidevercolor=#1d202100 --insidewrongcolor=#cc241dcc --ringcolor=#689d6aff --ringvercolor=#689d6a00 --ringwrongcolor=#fb4934ff --keyhlcolor=#b16286ff --bshlcolor=#d79921ff --verifcolor=#00000000 --wrongcolor=#00000000 --line-uses-inside --ring-width 5 --pass-media-keys --separatorcolor=#00000000";
+      lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color -n -c 1d2021 --indicator -k --time-color=#ebdbb2ff --date-color=#ebdbb2ff --inside-color=#1d2021cc --insidever-color=#1d202100 --insidewrong-color=#cc241dcc --ring-color=#689d6aff --ringver-color=#689d6a00 --ringwrong-color=#fb4934ff --keyhl-color=#b16286ff --bshl-color=#d79921ff --verif-color=#00000000 --wrong-color=#00000000 --line-uses-inside --ring-width 5 --pass-media-keys --separator-color=#00000000";
+      xautolock = {
+        enable = true;
+        detectSleep = true;
+      };
       xautolock.extraOptions = [
         "Xautolock.killer: systemctl suspend"
       ];
