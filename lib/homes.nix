c6e73f21faf91e4faf867604b980cae99357a6f7
Niklas Voss <niklas.voss@gmail.com>
2022-07-05 13:41:00 +0200

    Fix home-manager standalone setup

--- lib/homes.nix
+++ lib/homes.nix
@@ -1,7 +1,7 @@
-{ inputs, lib, pkgs, ... }:
+{ inputs, lib, mylib, pkgs, ... }:
 
 with lib;
-with lib.my;
+with mylib;
 let sys = "x86_64-linux";
 in
 {
@@ -10,10 +10,6 @@ in
       username = removeSuffix ".nix" (baseNameOf path);
       homeDirectory = "/home/${username}";
       defaults = { config, pkgs, lib, ... }: {
-        imports =
-          # All my personal modules
-          (mapModulesRec' (toString ../modules/home) import);
-
         programs.home-manager.enable = true;
         xdg.enable = true;
         xdg.mime.enable = true;
@@ -27,20 +23,16 @@ in
         home.username = username;
         home.homeDirectory = homeDirectory;
       };
-      configuration = { ... }: {
-        nixpkgs.pkgs = pkgs;
-        imports = [ ];
-      };
     in
     inputs.home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
-      modules = [
+      modules = (mapModulesRec' (toString ../modules/home) import) ++ [
         defaults
         (filterAttrs (n: v: !elem n [ "system" ]) attrs)
         (import path)
       ];
       extraSpecialArgs = {
-        inherit lib inputs system;
+        inherit inputs system mylib;
       };
     };
 
