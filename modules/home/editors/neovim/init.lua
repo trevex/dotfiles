56cb0bddd811343d71326b46897d6eb43c500937
Niklas Voss <niklas.voss@gmail.com>
2023-10-02 13:19:55 +0200

    Only explicit select in cmp

--- modules/home/editors/neovim/init.lua
+++ modules/home/editors/neovim/init.lua
@@ -328,7 +328,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
